7ff4c2645408f4905f9eaea7083a145215f9d2fe
strider29 <ravjituppal@gmail.com>
2014-07-08 23:05:37 +0530

    removed un-neccesary code and added search icon

--- app/src/main/java/droiders/dark/seenbeenread/activities/BaseActivity.java
+++ app/src/main/java/droiders/dark/seenbeenread/activities/BaseActivity.java
@@ -1,9 +1,12 @@
 package droiders.dark.seenbeenread.activities;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
@@ -12,13 +15,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.Toast;
 
+import droiders.dark.seenbeenread.menufragments.BooksFragment;
+import droiders.dark.seenbeenread.menufragments.MoviesFragment;
+import droiders.dark.seenbeenread.menufragments.PlacesFragment;
 import droiders.dark.seenbeenread.navigationdrawer.NavigationDrawerFragment;
 import droiders.dark.seenbeenread.R;
 
 
 public class BaseActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks, SearchView.OnQueryTextListener {
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
@@ -28,7 +36,7 @@ public class BaseActivity extends ActionBarActivity
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
      */
-    private CharSequence mTitle;
+    private CharSequence mTitle="Movies";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,20 +51,29 @@ public class BaseActivity extends ActionBarActivity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
     }
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
-        Fragment menuFragment;
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        ActionBar actionBar = getSupportActionBar();
+        setTitle(position+1);
+        actionBar.setTitle(mTitle);
+        Fragment menuFragment=null;
+        if(position==0)
+            menuFragment=new MoviesFragment();
+        else if(position==1)
+            menuFragment=new BooksFragment();
+        else if(position==2)
+            menuFragment=new PlacesFragment();
+        FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .replace(R.id.container, menuFragment)
                 .commit();
     }
 
-    public void onSectionAttached(int number) {
+    public void setTitle(int number) {
         switch (number) {
             case 1:
                 mTitle =  "Movies";
@@ -94,9 +111,12 @@ public class BaseActivity extends ActionBarActivity
             // if the drawer is not showing. Otherwise, let the drawer
             // decide what to show in the action bar.
             getMenuInflater().inflate(R.menu.base, menu);
+            SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+            searchView.setOnQueryTextListener(this);
             restoreActionBar();
             return true;
         }
+
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -112,44 +132,18 @@ public class BaseActivity extends ActionBarActivity
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_base, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((BaseActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Toast.makeText(this, "Searching for: " + query + "...", Toast.LENGTH_SHORT).show();
+        return true;
     }
 
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        newText = newText.isEmpty() ? "" : "Query so far: " + newText;
+        Toast.makeText(this, newText, Toast.LENGTH_SHORT).show();
+
+        return true;
+
+    }
 }
--- app/src/main/java/droiders/dark/seenbeenread/navigationdrawer/NavigationDrawerFragment.java
+++ app/src/main/java/droiders/dark/seenbeenread/navigationdrawer/NavigationDrawerFragment.java
@@ -272,10 +272,10 @@ public class NavigationDrawerFragment extends Fragment {
             return true;
         }
 
-        if (item.getItemId() == R.id.action_example) {
+     /*   if (item.getItemId() == R.id.action_search) {
             Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
